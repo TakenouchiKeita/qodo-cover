dee9c10714aec9da96370eb8e074c7d90adbee07
Takenouchi Keita <t.keita.osaka@gmail.com>
2025-02-27 10:21:10 +0900

    Add support for Azure models in AICaller

--- cover_agent/AICaller.py
+++ cover_agent/AICaller.py
@@ -66,7 +66,7 @@ class AICaller:
         if prompt["system"] == "":
             messages = [{"role": "user", "content": prompt["user"]}]
         else:
-            if self.model in ["o1-preview", "o1-mini"]:
+            if self.model in ["o1-preview", "o1-mini", "azure/o1-preview", "azure/o1-mini"]:
                 # o1 doesn't accept a system message so we add it to the prompt
                 messages = [
                     {
@@ -90,7 +90,7 @@ class AICaller:
         }
 
         # Model-specific adjustments
-        if self.model in ["o1-preview", "o1-mini", "o1", "o3-mini"]:
+        if self.model in ["o1-preview", "o1-mini", "o1", "o3-mini", "azure/o1-preview", "azure/o1-mini"]:
             stream = False  # o1 doesn't support streaming
             completion_params["temperature"] = 1
             completion_params["stream"] = False  # o1 doesn't support streaming
